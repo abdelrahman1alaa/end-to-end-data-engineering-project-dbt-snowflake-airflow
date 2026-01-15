86852e96494db8ab73162cc96bfdf2417e33f63b
AbdelRahman Alaa <162742130+abdelrahman1alaa@users.noreply.github.com>
2026-01-15 06:08:32 +0200

    Delete marts directory

--- marts/fct_daily_order_revenue.sql
+++
@@ -1,9 +0,0 @@
-select 
-o.order_date, 
-o.order_id, 
-sum(total_price) as total_price
-from 
-{{ref('stg_orders')}} o   -- stg is a model , when i use modele i use ref
-LEFT JOIN {{ref('stg_order_items')}} OI 
-ON o.order_id=OI.order_id
-group by 1,2
